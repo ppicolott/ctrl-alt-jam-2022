a288008c59fd99236f6f9916dfe6028219169a0e
Paula Picolott <paula_picolott@hotmail.com>
2022-09-03 01:39:11 -0300

    Criação da prefab do player, replanejamento da plataforma vertical e organização do projeto

--- Assets/Scripts/Platform/VerticalPlatform.cs
+++ Assets/Scripts/Platform/VerticalPlatform.cs
@@ -4,22 +4,19 @@ using UnityEngine;
 
 public class VerticalPlatform : MonoBehaviour
 {
-    public float timer = 1f;
-    void FixedUpdate()
+    private void Start()
     {
-        timer -= Time.deltaTime;
-        if (timer <= 1f)
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 1f, 0);
-        }
-        if (timer <= 0)
-        {
-            GetComponent<Rigidbody2D>().velocity *= -1;
-        }
-        if(timer <= -1f)
+        GetComponent<Rigidbody2D>().velocity = new Vector3(0, 1f, 0);
+    }
+    private void FixedUpdate()
+    {
+        if(transform.position.y >= 1f)
         {
             GetComponent<Rigidbody2D>().velocity *= -1;
-            timer = 1f;
+        }
+        if(transform.position.y <= -0.15f)
+        {
+            GetComponent<Rigidbody2D>().velocity *= -1;
         }
     }
 }
