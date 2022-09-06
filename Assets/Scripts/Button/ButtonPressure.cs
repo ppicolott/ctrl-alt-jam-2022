4ff567d43babfaebeb8abdaabd6f4f603973dfe8
Leandro Shoiti <shoiti.tokunaga@gmail.com>
2022-09-06 20:05:36 -0300

    button: prefab

--- Assets/Scripts/Button/ButtonPressure.cs
+++ Assets/Scripts/Button/ButtonPressure.cs
@@ -3,16 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
-public class PresureButton : MonoBehaviour
+public class ButtonPressure : MonoBehaviour
 {
-    [SerializeField] private bool press;
+    private bool press;
+    private Animator animator;
+    void Start()
+    {
+        this.animator = GetComponent<Animator>();
+    }
     private void OnTriggerStay2D(Collider2D collision)
     {
+        this.animator.SetBool("Pressed", true);
         this.press = true;
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
+        this.animator.SetBool("Pressed", false);
         this.press = false;
     }
     public bool getPress()
