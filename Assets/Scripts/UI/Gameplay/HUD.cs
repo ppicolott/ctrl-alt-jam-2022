48ca76984a5cfecc32601f14802f73449cc7d6ac
Leandro Shoiti <shoiti.tokunaga@gmail.com>
2022-09-10 21:44:10 -0300

    hud: life

--- Assets/Scripts/UI/Gameplay/HUD.cs
+++ Assets/Scripts/UI/Gameplay/HUD.cs
@@ -15,7 +15,6 @@ public class HUD : MonoBehaviour
     private void Start()
     {
         current = this;
-        damage = 0f;
         width = 400f;
         height = 50f;
         iceLife.rectTransform.sizeDelta -= new Vector2(width, height);
@@ -23,19 +22,6 @@ public class HUD : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (width > 0 && damage > 0)
-        {
-            width -= damage;
-        }
-        if (width < 400f && damage < 0)
-        {
-            width -= damage;
-        }
-        iceLife.rectTransform.sizeDelta = new Vector2(width, height);
-
-        if (width <= 0)
-        {
-            SceneManager.LoadScene("GameOver");
-        }
+        iceLife.rectTransform.sizeDelta = new Vector2(width * ManagerStates.life, height);
     }
 }
